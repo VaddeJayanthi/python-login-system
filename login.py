ecabc6b1b708c7c598c4e0be6c25aa39bb6fa761
VaddeJayanthi <vjayanti689@gmail.com>
2026-09-21 11:30:17 +0530

    Add simple login functionality in login.py

---
+++ login.py
@@ -0,0 +1,12 @@
+username = input("Enter username: ")
+password = input("Enter password: ")
+
+if username == "admin" and password == "1234":
+    print("Login successful!")
+else:
+    print("Invalid username or password.")
+
+OUTPUT:
+Enter username: admin
+Enter password: 1234
+Login successful!
